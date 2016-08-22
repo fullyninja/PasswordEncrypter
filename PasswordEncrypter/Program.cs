4933751f0a0cbde62b46df5809aeb0ab0c176a7a
Peter Evans <peter@fullyninja.com>
2016-08-22 15:09:05 +1000

    - Parsing command line for plaintext password. Outputting encrypted password to stdout.

--- PasswordEncrypter/Program.cs
+++ PasswordEncrypter/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PasswordEncrypter.Security;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,21 @@ namespace PasswordEncrypter
     {
         static void Main(string[] args)
         {
-            // Test
+            string password = "";
+
+            if (args.Count() == 1)
+            {
+                password = args[0];
+            }
+            else
+            { 
+                Console.WriteLine("PasswordEncrypter.exe <password>");
+            }
+
+            var secPassword = StringEncrypter.ToSecureString(password);
+            string encryptedPassword = StringEncrypter.EncryptString(secPassword);
+
+            Console.WriteLine(encryptedPassword);
         }
     }
 }
